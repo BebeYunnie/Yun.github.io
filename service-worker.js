5b66f263b8ca00b7a4dac06c018791f73deaf299
BebeYunnie <bebeyunie@gmail.com>
2025-03-25 16:16:03 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,23 @@
-const CACHE_NAME = 'quote-app-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/script.js',
-    '/manifest.json',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
-];
-
-// 安裝 Service Worker
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache);
+        caches.open('quote-app-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/script.js',
+                '/styles.css',
+                '/manifest.json',
+                '/icons/icon-192x192.png',
+                '/icons/icon-512x512.png'
+            ]);
         })
     );
 });
 
-// 啟用 Service Worker 並清除舊快取
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.filter(cache => cache !== CACHE_NAME).map(cache => caches.delete(cache))
-            );
-        })
-    );
-});
-
-// 攔截網路請求，提供快取內容
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
         })
     );
 });
